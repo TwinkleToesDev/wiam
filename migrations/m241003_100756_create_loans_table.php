08c4232ebd9e28652676f1f10a0dee8d34618e7e
Maksim <112960156+TwinkleToesDev@users.noreply.github.com>
2024-10-04 15:27:44 +0400

    migration

---
+++ migrations/m241003_100756_create_loans_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%loans}}`.
+ */
+class m241003_100756_create_loans_table extends Migration
+{
+
+    /**
+     * Creates the `loan_request` table
+     *
+     * The table has the following columns:
+     * - `id`: Primary key.
+     * - `user_id`: Integer, not null.
+     * - `amount`: Integer, not null.
+     * - `term`: Integer, not null.
+     * - `status`: String, not null, defaults to 'pending'.
+     * - `created_at`: Timestamp, defaults to the current timestamp.
+     *
+     * An index is created on the `user_id` column
+     */
+    public function safeUp()
+    {
+        $this->createTable('loan_request', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'amount' => $this->integer()->notNull(),
+            'term' => $this->integer()->notNull(),
+            'status' => $this->string()->notNull()->defaultValue('pending'),
+            'created_at' => $this->timestamp()->defaultExpression('NOW()'),
+        ]);
+
+        $this->createIndex('idx-loan_request-user_id', '{{%loan_request}}', 'user_id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%loans}}');
+    }
+}
